2ab88fcf275388b8627702523bdb9213395dd35c
AC De Leon <andrewchristopherdeleon@gmail.com>
2019-04-08 03:21:37 -0700

    [+] /public/

---
+++ public/precache-manifest.d1086bb7847a0f4e69bb98167f484a7c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1983b218b03e878eb353968a99c6dae5",
+    "url": "/static/media/logo.1983b218.png"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "aaa3081ab0d38d7a4e20",
+    "url": "/static/js/main.18b7cac4.chunk.js"
+  },
+  {
+    "revision": "962b1265828958a3defd",
+    "url": "/static/js/2.5f000af4.chunk.js"
+  },
+  {
+    "revision": "aaa3081ab0d38d7a4e20",
+    "url": "/static/css/main.5d75621f.chunk.css"
+  },
+  {
+    "revision": "3a3e35bf680eadf5863969fb656fd6ce",
+    "url": "/index.html"
+  }
+];
