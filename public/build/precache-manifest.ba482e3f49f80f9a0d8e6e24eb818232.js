2ec829c620012df6cad566c1357b2820a9be5b0c
AC De Leon <aadeleon@sdsu.edu>
2019-04-08 14:36:48 -0700

    [*] /public/

---
+++ public/build/precache-manifest.ba482e3f49f80f9a0d8e6e24eb818232.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1983b218b03e878eb353968a99c6dae5",
+    "url": "/static/media/logo.1983b218.png"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "a6727b8e7b51cba12a6f",
+    "url": "/static/js/main.85554d91.chunk.js"
+  },
+  {
+    "revision": "962b1265828958a3defd",
+    "url": "/static/js/2.5f000af4.chunk.js"
+  },
+  {
+    "revision": "a6727b8e7b51cba12a6f",
+    "url": "/static/css/main.a90d569b.chunk.css"
+  },
+  {
+    "revision": "e3a0b734c3e4645747ad0baa73de6990",
+    "url": "/index.html"
+  }
+];
